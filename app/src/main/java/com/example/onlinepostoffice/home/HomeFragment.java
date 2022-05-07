f4011c72eec80e2a74c5aaa9c9fca87164578463
Mengnali_Dou <1004718475@qq.com>
2022-05-07 19:20:30 +0800

    22-05-07

--- app/src/main/java/com/example/onlinepostoffice/home/HomeFragment.java
+++ app/src/main/java/com/example/onlinepostoffice/home/HomeFragment.java
@@ -11,12 +11,14 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.onlinepostoffice.R;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel mViewModel;
+    private Button btnDynamicState;
 
     public static HomeFragment newInstance() {
         return new HomeFragment();
@@ -35,4 +37,16 @@ public class HomeFragment extends Fragment {
         // TODO: Use the ViewModel
     }
 
+//    @Override
+//    public void onCreate(@Nullable Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        btnDynamicState=(Button) btnDynamicState.findViewById(R.id.btnDynamicState);
+//        String[] dynamicState = {"关于2022年5月新飞行纪特邮票预售安排的公告",
+//                "4月上新日【江山邮画】",
+//                "关于调整《中墨建交五十周年》邮票预售自提相关时限",
+//                "4月优选日【邮古说今】",
+//                "中国古典文学名著--《红楼梦》（全张邮票折）摇号",
+//                "关于2022年4月新发型纪特邮票预售安排的公告"};
+//        btnDynamicState.setText(dynamicState[0]);
+//    }
 }
